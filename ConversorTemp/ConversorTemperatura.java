b71f13a5cb4b6daa3d0de0af2aea70d34cf6f4d8
GabrielProBS <gabriel.silva3804@etec.sp.gov.br>
2025-03-30 17:59:39 -0300

    Update ConversorTemperatura.java

--- ConversorTemp/ConversorTemperatura.java
+++ ConversorTemp/ConversorTemperatura.java
@@ -1,11 +1,11 @@
 public class Conversor {
     public double conversor(double celsius){
-        return celsius * 9/5 + 32;
+        return celsius * 9/5 + 32; //Cálculo de conversão de Celsius para Fahrenheit
     }
 
     public float conversor(float fahrenheit){
-        return (fahrenheit - 32) * 5/9;
+        return (fahrenheit - 32) * 5/9; //Cálculo de conversão de Fahrenheit para Celsius
     }
 
-public Conversor(){}
+public Conversor(){} //Para deixar global
 }
