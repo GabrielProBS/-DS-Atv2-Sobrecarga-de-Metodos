8f7ba8da764340f6c741363e8403c6917508ff6d
GabrielProBS <gabriel.silva3804@etec.sp.gov.br>
2025-03-30 17:55:47 -0300

    ConversorTemperatura.java

--- ConversorTemp/ConversorTemperatura.java
+++ ConversorTemp/ConversorTemperatura.java
@@ -1,10 +1,11 @@
-public double conversor(double celsius){
-    double fahrenheit = (celsius * 9/5) + 32;
-    return fahrenheit;
-}
+public class Conversor {
+    public double conversor(double celsius){
+        return celsius * 9/5 + 32;
+    }
 
-public double conversor(double fahrenheit){
-    double celsius = (fahrenheit - 32) * 5/9;
-    return celsius;
-}
+    public float conversor(float fahrenheit){
+        return (fahrenheit - 32) * 5/9;
+    }
 
+public Conversor(){}
+}
